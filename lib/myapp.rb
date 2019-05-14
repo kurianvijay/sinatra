90449f9a125b3b06147017ff18d703b7e2cbabb5
Vijay Kurian <kurianvijay7@gmail.com>
2019-05-14 16:13:31 +0100

    Add post method and cat name form erb

--- lib/myapp.rb
+++ lib/myapp.rb
@@ -30,8 +30,12 @@ get '/random-cat'do
 erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
 @name = params[:name]
 erb(:index)
+end
+
+get '/cat-name-form' do
+  erb(:cat_name_form)
 end
